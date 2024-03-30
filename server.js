9cfbc68dd3dc2045913651728d29800fd38e4bc3
mahnsi <mahnsir22@gmail.com>
2024-03-30 17:31:38 -0400

    database nodejs

---
+++ server.js
@@ -0,0 +1,34 @@
+const {MongoClient} = require('mongodb');
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://mahnsirupa10:<password>@cluster0.ncsejux.mongodb.net/"
+
+
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
