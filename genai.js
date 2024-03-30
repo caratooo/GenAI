bed9e869ab5c20e95d4c93302f89b8f0a36e5361
mahnsi <mahnsir22@gmail.com>
2024-03-30 18:49:32 -0400

    image display on upload

--- genai.js
+++ genai.js
@@ -1,26 +1,44 @@
+// Event listener for html page load (this function is called when the html page is loaded)
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("upload-form").addEventListener("submit", function(event) {
-        event.preventDefault();
-        var formData = new FormData(this);
-
-        fetch("/upload", {
-            method: "POST",
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            // Display transcribed text
-            document.getElementById("result-container").innerHTML = `
-                <h2>Transcribed Text:</h2>
-                <p>${data.transcribed_text}</p>
-                <h2>Paraphrased Text:</h2>
-                <p>${data.paraphrased_result}</p>
-                <h2>Summarized Text:</h2>
-                <p>${data.summarized_result}</p>
-            `;
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
-    });
+    const fileInput = document.getElementById("mediaFile");
+    if (fileInput) {
+        // Event listener for file input change
+        // (whenever a change is made to "audioFile" from the html, the function "handleFileSelect" is called)
+        fileInput.addEventListener("change", handleFileSelect);
+    } else {
+        console.error("Element with ID 'mediaFile' not found.");
+    }
 });
+
+
+// handle file selection
+function handleFileSelect(event) {
+    const fileInput = document.getElementById("mediaFile");
+    if (!fileInput) {
+        console.error("Element with ID 'mediaFile' not found.");
+        return;
+    }
+    const file = event.target.files[0]; // Get the selected file
+    
+
+    //print the file name to the console for now
+    console.log("Selected image file:", file);
+}
+
+function previewImage() {
+    var input = document.getElementById('mediaFile');
+    var preview = document.getElementById('image-preview');
+    
+    input.addEventListener('change', function() {
+        var file = input.files[0];
+        var reader = new FileReader();
+        
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+        }
+        
+        reader.readAsDataURL(file);
+    });
+}
+
+previewImage();
